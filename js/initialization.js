29d40c83209c717e7abc770f2be060e0ec9d3212
dyl25 <dylan-vst@hotmail.com>
2020-12-13 14:20:29 +0100

    add mobile sidenav

---
+++ js/initialization.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.sidenav');
+
+    let options = {
+        edge: 'right'
+    }
+
+    var instances = M.Sidenav.init(elems, options);
+});
